5674aa75876e7969e09e311a58db5459561ea4fe
Efthimis Charitonidis <efthimis@charitonidis.xyz>
2023-02-05 17:53:48 +0200

    Update packages

--- MediatorApiExample/Behaviors/ValidationBehavior.cs
+++ MediatorApiExample/Behaviors/ValidationBehavior.cs
@@ -17,26 +17,26 @@ namespace MediatorApiExample.Behaviors
             _validators = validators;
         }
 
-        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
             var context = new ValidationContext<TRequest>(request);
-            var failures = _validators
-                .Select(x => x.Validate(context))
-                .SelectMany(x => x.Errors)
-                .Where(x => x != null)
-                .ToList();
+                var failures = _validators
+                    .Select(x => x.Validate(context))
+                    .SelectMany(x => x.Errors)
+                    .Where(x => x != null)
+                    .ToList();
 
-            if (failures.Count > 0)
-            {
+                if (failures.Count > 0)
+                {
 
+                }
+
+                // pre
+
+                return next();
+
+                // post
             }
-
-            // pre
-
-            return next();
-
-            // post
         }
-    }
 }
 
